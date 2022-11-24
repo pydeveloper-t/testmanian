5460d8832aa22d0f8c58c0a801da46e296c0d9c2
pydeveloper-t <64210897+pydeveloper-t@users.noreply.github.com>
2022-11-24 19:27:48 +0200

    Update build_docker.sh
    Added code from run.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+git clone https://github.com/pydeveloper-t/testmanian.git .
+cd ./testmanian
+mkdir /work/testmanian
+cp ./config.yaml /work/testmanian
+sh ./build_docker.sh
 docker_image_name="py:testmanian"
 docker stop $(docker ps -a -q --filter ancestor=$docker_image_name)
 docker rm $(docker ps -a -q --filter ancestor=$docker_image_name)
