7de80d52e3aa23110ee06030e81146bfa7c3f2d5
pydeveloper-t <64210897+pydeveloper-t@users.noreply.github.com>
2022-11-24 19:26:49 +0200

    Update run.sh
    part of code moved into build_docker.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-git clone https://github.com/pydeveloper-t/testmanian.git .
-cd ./testmanian
-mkdir /work/testmanian
-cp ./config.yaml /work/testmanian
-sh ./build_docker.sh
+
 docker stop testmanian
 docker rm testmanian
 docker run -d  --name testmanian  --net=host  -e testmanian_cfg=/work/testmanian/config.yaml  -v /work:/work py:testmanian --timeout 15
